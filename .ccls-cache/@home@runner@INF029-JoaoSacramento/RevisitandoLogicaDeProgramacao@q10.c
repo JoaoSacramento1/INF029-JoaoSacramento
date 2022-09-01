474fb24c614d47b347a21a9a0ca8a8fda151f820
João Sacramento <111467325+JoaoSacramento1@users.noreply.github.com>
2022-09-01 19:53:49 +0000

    Correção da questão 10

--- .ccls-cache/@home@runner@INF029-JoaoSacramento/RevisitandoLogicaDeProgramacao@q10.c
+++ .ccls-cache/@home@runner@INF029-JoaoSacramento/RevisitandoLogicaDeProgramacao@q10.c
@@ -6,7 +6,7 @@ typedef struct dados_pessoas{
   char nome[40];
   int idade;
   char sexo;
-  int cpf[10];
+  char cpf[16];
   } pessoas;
 
 int main(){
@@ -19,16 +19,16 @@ int main(){
     if (pessoa[i].nome[ln] == '\n')
       pessoa[i].nome[ln] = '\0'; // caracter de final de string
 
-    printf("Digite a idade: ");
-    scanf("%d", &pessoa[i].idade);
+    printf("Digite o sexo(m/f): ");
+    scanf("%c", &pessoa[i].sexo);
 
-    printf("Digite o sexo: ");
-    scanf("%c",&pessoa[i].sexo);
+    printf("Digite a idade: ");
+    scanf("%d",&pessoa[i].idade);
 
     getchar();
     
     printf("Digite o cpf: ");
-    fgets(pessoa[i].cpf, 10, stdin); 
+    fgets(pessoa[i].cpf, 16, stdin); 
     ln = strlen(pessoa[i].cpf) - 1; 
     if (pessoa[i].cpf[ln] == '\n')
       pessoa[i].cpf[ln] = '\0';
